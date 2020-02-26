e2d0a3e9b6b8d006e54ad11960d9b846a2ceb6a3
wangyizhou110 <61504802+wangyizhou110@users.noreply.github.com>
2020-02-26 13:18:41 +0000

    Create calculate.h

---
+++ question 1/calculate.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATE_H
+#define CALCULATE_H
+
+#include<iostream>
+#include <cstring>
+using namespace std;
+
+
+void calculate(int &count, int count1, int count2, int num1[], int num2[], int num3[]);
+
+
+
+#endif 
